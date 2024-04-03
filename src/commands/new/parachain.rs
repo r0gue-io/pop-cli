668ca50498189a509a0d34eb4d1fab705f151e88
AlexD10S <alexfraga10@gmail.com>
2024-04-03 11:52:30 +0200

    feat: guide user in create parachain process

--- src/commands/new/parachain.rs
+++ src/commands/new/parachain.rs
@@ -3,11 +3,12 @@ use crate::{
 	helpers::git_init,
 	style::{style, Theme},
 };
+use anyhow::Result;
 use clap::{Args, Parser};
 use std::{fs, path::Path};
 use strum_macros::{Display, EnumString};
 
-use cliclack::{clear_screen, confirm, intro, outro, outro_cancel, set_theme};
+use cliclack::{clear_screen, confirm, input, intro, outro, outro_cancel, set_theme};
 
 #[derive(Clone, Parser, Debug, Display, EnumString, PartialEq)]
 pub enum Template {
@@ -30,6 +31,15 @@ impl Template {
 			},
 		}
 	}
+	fn from(provider_name: &str) -> Self {
+		match provider_name {
+			"base" => Template::Base,
+			"template" => Template::OZTemplate,
+			"cpt" => Template::ParityContracts,
+			"fpt" => Template::ParityFPT,
+			_ => Template::Base,
+		}
+	}
 }
 
 #[derive(Clone, Default, Parser, Debug, Display, EnumString, PartialEq)]
@@ -50,12 +60,50 @@ impl Provider {
 			Provider::Parity => return Template::ParityContracts,
 		}
 	}
+	fn from(provider_name: &str) -> Self {
+		match provider_name {
+			"Pop" => Provider::Pop,
+			"OpenZeppelin" => Provider::OpenZeppelin,
+			"Parity" => Provider::Parity,
+			_ => Provider::Pop,
+		}
+	}
+	fn display_select_options(&self) -> &str {
+		match &self {
+			Provider::Pop => {
+				return cliclack::select(format!("Select a template provider: "))
+					.initial_value("base")
+					.item("base", "Base Parachain", "A standard parachain")
+					.interact()
+					.expect("Error parsing user input");
+			},
+			Provider::OpenZeppelin => {
+				return cliclack::select(format!("Select a template provider: "))
+					.initial_value("template")
+					.item(
+						"template",
+						"OpenZeppeling Template",
+						"OpenZeppeling Runtime Parachain Template",
+					)
+					.interact()
+					.expect("Error parsing user input");
+			},
+			Provider::Parity => {
+				return cliclack::select(format!("Select a template provider: "))
+					.initial_value("cpt")
+					.item("cpt", "Parity Contracts", "A parachain supporting WebAssembly smart contracts such as ink!.")
+					.item("fpt", "Parity EVM", "A parachain supporting smart contracts targeting the Ethereum Virtual Machine (EVM).")
+					.interact()
+					.expect("Error parsing user input");
+			},
+		};
+	}
 }
 
 #[derive(Args)]
 pub struct NewParachainCommand {
 	#[arg(help = "Name of the project. Also works as a directory path for your project")]
-	pub(crate) name: String,
+	pub(crate) name: Option<String>,
 	#[arg(help = "Provider to pick template: Options are pop, openzeppelin and parity.")]
 	#[arg(default_value = "pop")]
 	pub(crate) provider: Option<Provider>,
@@ -77,61 +125,104 @@ pub struct NewParachainCommand {
 }
 
 impl NewParachainCommand {
-	pub(crate) fn execute(&self) -> anyhow::Result<()> {
+	pub(crate) fn execute(&self) -> Result<()> {
 		clear_screen()?;
-		let provider = &self.provider.clone().unwrap_or(Provider::Pop);
-		let template = &self.template.clone().unwrap_or(provider.default_template());
-		if !template.is_provider_correct(provider) {
-			outro_cancel(format!(
-				"The provider \"{}\" doesn't support the {} template.",
-				provider, template
-			))?;
-			return Ok(());
-		}
-		intro(format!(
-			"{}: Generating \"{}\" using {} from {}!",
-			style(" Pop CLI ").black().on_magenta(),
-			&self.name,
-			template,
-			provider
-		))?;
-		set_theme(Theme);
-		let destination_path = Path::new(&self.name);
-		if destination_path.exists() {
-			if !confirm(format!(
-				"\"{}\" directory already exists. Would you like to remove it?",
-				destination_path.display()
-			))
-			.interact()?
-			{
+		if let Some(name_template) = &self.name {
+			let provider = &self.provider.clone().unwrap_or(Provider::Pop);
+			let template = &self.template.clone().unwrap_or(provider.default_template());
+			if !template.is_provider_correct(provider) {
 				outro_cancel(format!(
-					"Cannot generate parachain until \"{}\" directory is removed.",
-					destination_path.display()
+					"The provider \"{}\" doesn't support the {} template.",
+					provider, template
 				))?;
 				return Ok(());
-			}
-			fs::remove_dir_all(destination_path)?;
-		}
-		let mut spinner = cliclack::spinner();
-		spinner.start("Generating parachain...");
-		instantiate_template_dir(
-			template,
-			destination_path,
-			Config {
+			};
+			let config = Config {
 				symbol: self.symbol.clone().expect("default values"),
 				decimals: self.decimals.clone().expect("default values"),
 				initial_endowment: self.initial_endowment.clone().expect("default values"),
-			},
-		)?;
-		if let Err(err) = git_init(destination_path, "initialized parachain") {
-			if err.class() == git2::ErrorClass::Config && err.code() == git2::ErrorCode::NotFound {
-				outro_cancel("git signature could not be found. Please configure your git config with your name and email")?;
-			}
+			};
+			generate_template(name_template, provider, template, config)?
+		} else {
+			guide_user()?;
 		}
-		spinner.stop("Generation complete");
-		outro(format!("cd into \"{}\" and enjoy hacking! 🚀", &self.name))?;
 		Ok(())
 	}
+}
+
+fn guide_user() -> Result<()> {
+	let name: String = input("Where should your project be created?")
+		.placeholder("my-parachain")
+		.interact()?;
+
+	let provider_name = cliclack::select(format!("Select a template provider: "))
+		.initial_value("Pop")
+		.item("Pop", "Pop", "An all-in-one tool for Polkadot development. 1 available options")
+		.item(
+			"OpenZeppelin",
+			"OpenZeppelin",
+			"The standard for secure blockchain applications. 1 available options",
+		)
+		.item("Parity", "Parity", "Solutions for a trust-free world. 2 available options")
+		.interact()?;
+
+	let provider = Provider::from(provider_name);
+	let template_name = provider.display_select_options();
+	let template = Template::from(template_name);
+
+	generate_template(
+		&name,
+		&provider,
+		&template,
+		Config {
+			symbol: "UNIT".to_string(),
+			decimals: 12,
+			initial_endowment: "1u64 << 60".to_string(),
+		},
+	)
+}
+
+fn generate_template(
+	name_template: &String,
+	provider: &Provider,
+	template: &Template,
+	config: Config,
+) -> Result<()> {
+	intro(format!(
+		"{}: Generating \"{}\" using {} from {}!",
+		style(" Pop CLI ").black().on_magenta(),
+		name_template,
+		template,
+		provider
+	))?;
+	set_theme(Theme);
+	let destination_path = Path::new(name_template);
+	if destination_path.exists() {
+		if !confirm(format!(
+			"\"{}\" directory already exists. Would you like to remove it?",
+			destination_path.display()
+		))
+		.interact()?
+		{
+			outro_cancel(format!(
+				"Cannot generate parachain until \"{}\" directory is removed.",
+				destination_path.display()
+			))?;
+			return Ok(());
+		}
+		fs::remove_dir_all(destination_path)?;
+	}
+	let mut spinner = cliclack::spinner();
+	spinner.start("Generating parachain...");
+	instantiate_template_dir(template, destination_path, config)?;
+	if let Err(err) = git_init(destination_path, "initialized parachain") {
+		if err.class() == git2::ErrorClass::Config && err.code() == git2::ErrorCode::NotFound {
+			outro_cancel("git signature could not be found. Please configure your git config with your name and email")?;
+		}
+	}
+	spinner.stop("Generation complete");
+	outro(format!("cd into \"{}\" and enjoy hacking! 🚀", name_template))?;
+	Ok(())
 }
 
 #[cfg(test)]
@@ -145,7 +236,7 @@ mod tests {
 	#[test]
 	fn test_new_parachain_command_execute() -> anyhow::Result<()> {
 		let command = NewParachainCommand {
-			name: "test_parachain".to_string(),
+			name: Some("test_parachain".to_string()),
 			provider: Some(Provider::Pop),
 			template: Some(Template::Base),
 			symbol: Some("UNIT".to_string()),
@@ -156,7 +247,7 @@ mod tests {
 		assert!(result.is_ok());
 
 		// check for git_init
-		let repo = Repository::open(Path::new(&command.name))?;
+		let repo = Repository::open(Path::new(&command.name.unwrap()))?;
 		let reflog = repo.reflog("HEAD")?;
 		assert_eq!(reflog.len(), 1);
 
