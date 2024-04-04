5e237427ce506cffd6d827fb22a58f85b3d8f877
Abhishek Shah <abhishekshah3@gmail.com>
2024-04-04 18:11:23 +0530

    fixes: fix contract engine unit tests

--- src/engines/contract_engine.rs
+++ src/engines/contract_engine.rs
@@ -14,13 +14,15 @@ use sp_weights::Weight;
 use subxt::PolkadotConfig as DefaultConfig;
 use subxt_signer::sr25519::Keypair;
 
+/// Create a new smart contract at `target`
 pub fn create_smart_contract(name: String, target: &Path) -> anyhow::Result<()> {
 	// In this code, out_dir will automatically join `name` to `target`,
 	// which is created prior to the call to this function
 	// So we must pass `target.parent()`
-	new_contract_project(&name, target.parent())
+	new_contract_project(&name, target.canonicalize()?.parent())
 }
 
+/// Build a smart contract
 pub fn build_smart_contract(path: &Option<PathBuf>) -> anyhow::Result<()> {
 	// If the user specifies a path (which is not the current directory), it will have to manually
 	// add a Cargo.toml file. If not provided, pop-cli will ask the user for a specific path. or ask
@@ -178,29 +180,30 @@ mod tests {
 	use std::fs;
 
 	fn setup_test_environment() -> Result<tempfile::TempDir, Error> {
-		let temp_contract_dir = tempfile::tempdir().expect("Could not create temp dir");
-		let result: anyhow::Result<()> =
-			create_smart_contract("test_contract".to_string(), temp_contract_dir.path());
+		let temp_dir = tempfile::tempdir().expect("Could not create temp dir");
+		let temp_contract_dir = temp_dir.path().join("test_contract");
+		fs::create_dir(&temp_contract_dir)?;
+		let result =
+			create_smart_contract("test_contract".to_string(), temp_contract_dir.as_path());
+		assert!(result.is_ok(), "Contract test environment setup failed");
 
-		assert!(result.is_ok(), "Result should be Ok");
-
-		Ok(temp_contract_dir)
+		Ok(temp_dir)
 	}
 
 	#[test]
 	fn test_contract_create() -> Result<(), Error> {
-		let temp_contract_dir = setup_test_environment()?;
+		let temp_dir = setup_test_environment()?;
 
 		// Verify that the generated smart contract contains the expected content
 		let generated_file_content =
-			fs::read_to_string(temp_contract_dir.path().join("test_contract/lib.rs"))
+			fs::read_to_string(temp_dir.path().join("test_contract/lib.rs"))
 				.expect("Could not read file");
 
 		assert!(generated_file_content.contains("#[ink::contract]"));
 		assert!(generated_file_content.contains("mod test_contract {"));
 
 		// Verify that the generated Cargo.toml file contains the expected content
-		fs::read_to_string(temp_contract_dir.path().join("test_contract/Cargo.toml"))
+		fs::read_to_string(temp_dir.path().join("test_contract/Cargo.toml"))
 			.expect("Could not read file");
 		Ok(())
 	}
