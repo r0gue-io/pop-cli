4771cc7f1a6e984e99e5b411228f40a6af96c02c
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2024-05-18 23:47:39 +0100

    fix: convert build_path check to check on path

--- crates/pop-cli/src/commands/up/contract.rs
+++ crates/pop-cli/src/commands/up/contract.rs
@@ -61,7 +61,10 @@ impl UpContractCommand {
 				+ "/target/release/ink",
 		);
 
-		if !build_path.exists() {
+		if !build_path.as_path().exists() {
+			log::warning(format!(
+				"NOTE: contract has not yet been built."
+			))?;
 			intro(format!("{}: Building a contract", style(" Pop CLI ").black().on_magenta()))?;
 			// Directory exists, proceed with the rest of the code
 			let result = build_smart_contract(&self.path)?;
