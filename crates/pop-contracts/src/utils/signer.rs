a54c518e03f58b91fbd9bf82880b61e7896600ee
AlexD10S <alexfraga10@gmail.com>
2024-06-27 13:21:19 +0200

    test: add unit tests in contracts

--- crates/pop-contracts/src/utils/signer.rs
+++ crates/pop-contracts/src/utils/signer.rs
@@ -18,3 +18,39 @@ pub fn parse_hex_bytes(input: &str) -> Result<Bytes, Error> {
 	let bytes = decode_hex(input).map_err(|e| Error::HexParsing(format!("{}", e)))?;
 	Ok(bytes.into())
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use anyhow::Result;
+
+	#[test]
+	fn create_signer_works() -> Result<(), Error> {
+		let keypair = create_signer("//Alice")?;
+		assert_eq!(
+			keypair.public_key().to_account_id().to_string(),
+			"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY" //Alice account
+		);
+		Ok(())
+	}
+
+	#[test]
+	fn create_signer_fails_wrong_key() -> Result<(), Error> {
+		assert!(matches!(create_signer("11111"), Err(Error::KeyPairCreation(..))));
+		Ok(())
+	}
+
+	#[test]
+	fn parse_hex_bytes_works() -> Result<(), Error> {
+		let input_in_hex = "48656c6c6f";
+		let result = parse_hex_bytes(input_in_hex)?;
+		assert_eq!(result, Bytes(vec![72, 101, 108, 108, 111]));
+		Ok(())
+	}
+
+	#[test]
+	fn parse_hex_bytes_fails_wrong_input() -> Result<(), Error> {
+		assert!(matches!(parse_hex_bytes("wronghexvalue"), Err(Error::HexParsing(..))));
+		Ok(())
+	}
+}
