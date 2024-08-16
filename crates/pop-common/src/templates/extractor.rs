2ed4d7a1859ae4473ed66df67d0bcaa7c75ae3a2
AlexD10S <alexfraga10@gmail.com>
2024-08-16 13:25:21 +0200

    refactor: move extract_template_files into common crate

---
+++ crates/pop-common/src/templates/extractor.rs
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: GPL-3.0
+
+use anyhow::Result;
+use std::{fs, io, path::Path};
+
+/// Extracts the specified template files from the repository folder to the target folder.
+///
+/// # Arguments
+/// * `template_name` - The name of the template to extract.
+/// * `repo_folder` - The path to the repository folder containing the template.
+/// * `target_folder` - The destination path where the template files should be copied.
+/// * `ignore_folders` - A vector of folder names to ignore during the extraction. If empty, no folders are ignored.
+///
+pub fn extract_template_files(
+	template_name: String,
+	repo_folder: &Path,
+	target_folder: &Path,
+	ignore_folders: Option<Vec<String>>,
+) -> Result<()> {
+	let template_folder = repo_folder.join(template_name);
+	// Recursively copy all folders and files within. Ignores the specified ones.
+	copy_dir_all(&template_folder, target_folder, &ignore_folders.unwrap_or_else(|| vec![]))?;
+	Ok(())
+}
+
+/// Recursively copy a directory and its files.
+///
+/// # Arguments
+/// * `src`: - The source path of the directory to be copied.
+/// * `dst`: - The destination path where the directory and its contents will be copied.
+/// * `ignore_folders` - Folders to ignore during the copy process.
+///
+fn copy_dir_all(
+	src: impl AsRef<Path>,
+	dst: impl AsRef<Path>,
+	ignore_folders: &Vec<String>,
+) -> io::Result<()> {
+	fs::create_dir_all(&dst)?;
+	for entry in fs::read_dir(src)? {
+		let entry = entry?;
+		let ty = entry.file_type()?;
+		if ty.is_dir() && ignore_folders.contains(&entry.file_name().to_string_lossy().to_string())
+		{
+			continue;
+		} else if ty.is_dir() {
+			copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()), ignore_folders)?;
+		} else {
+			fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+		}
+	}
+	Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use anyhow::{Error, Result};
+	use std::fs;
+
+	fn generate_testing_contract(template: &str) -> Result<tempfile::TempDir, Error> {
+		let temp_dir = tempfile::tempdir()?;
+		let template_folder = temp_dir.path().join(template.to_string());
+		fs::create_dir(&template_folder)?;
+		fs::File::create(&template_folder.join("lib.rs"))?;
+		fs::File::create(&template_folder.join("Cargo.toml"))?;
+		fs::create_dir(&temp_dir.path().join("noise_folder"))?;
+		fs::create_dir(&template_folder.join("frontend"))?;
+		Ok(temp_dir)
+	}
+	#[test]
+	fn extract_template_files_works() -> Result<(), Error> {
+		// Contract
+		let mut temp_dir = generate_testing_contract("erc20")?;
+		let mut output_dir = tempfile::tempdir()?;
+		extract_template_files("erc20".to_string(), temp_dir.path(), output_dir.path(), None)?;
+		assert!(output_dir.path().join("lib.rs").exists());
+		assert!(output_dir.path().join("Cargo.toml").exists());
+		assert!(output_dir.path().join("frontend").exists());
+		assert!(!output_dir.path().join("noise_folder").exists());
+		// ignore the frontend folder
+		temp_dir = generate_testing_contract("erc721")?;
+		output_dir = tempfile::tempdir()?;
+		extract_template_files(
+			"erc721".to_string(),
+			temp_dir.path(),
+			output_dir.path(),
+			Some(vec!["frontend".to_string()]),
+		)?;
+		assert!(output_dir.path().join("lib.rs").exists());
+		assert!(output_dir.path().join("Cargo.toml").exists());
+		assert!(!output_dir.path().join("frontend").exists());
+		assert!(!output_dir.path().join("noise_folder").exists());
+
+		Ok(())
+	}
+}
