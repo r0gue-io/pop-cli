c53ff4556dfbbb0cf998b1dca182eb7bd7f08c31
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2024-08-19 15:48:08 +0200

    Update crates/pop-parachains/src/new_parachain.rs
    Co-authored-by: Frank Bell <60948618+evilrobot-01@users.noreply.github.com>

--- crates/pop-parachains/src/new_parachain.rs
+++ crates/pop-parachains/src/new_parachain.rs
@@ -84,7 +84,7 @@ pub fn instantiate_standard_template(
 ///
 /// # Arguments
 ///
-/// * `template` - template to generate the parachain from.
+/// * `template` - template to generate the container-chain from.
 /// * `target` - location where the parachain will be created.
 /// * `tag_version` - version to use (`None` to use latest).
 fn instantiate_tanssi_template(
