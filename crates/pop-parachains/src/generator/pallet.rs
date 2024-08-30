43b592b4e490d1d0f93e63cca543467962a8b6dd
Tomás Senovilla Polo <117524919+tsenovilla@users.noreply.github.com>
2024-08-30 12:09:35 +0200

    feat: improve pallet template generation (#261)
    * Interactive interface added
    
    * Update pick_options_and_give_name macro to handle an arbitrary amount of cases, allowing pick several options for the same object
    
    * New macro for multiselect from an enum. Improve the CLI to support more options
    
    * Adding the possibility of using custom origins
    
    * Adding Cargo.toml generation
    
    * Template lib.rs. Pending of review
    
    * Finishing lib (and related) templates
    
    * New mock config
    
    * Finishing templates
    
    * Adding some tests for the folder structure
    
    * Formatting repo
    
    * Solving mock implementation of common types when no default config issue
    
    * Adding more links to Polkadot-SDK-docs
    
    * Formatting lib.rs.templ
    
    * Solving small issues
    
    * Solving doc test issue, the struct used in that test was modified by this PR
    
    * Apply suggestions from code review
    
    Comment corrections by Bruno
    
    Co-authored-by: Bruno Galvao <brunopgalvao@gmail.com>
    
    * Adding simple/advanced modes
    
    * Adding advanced mode
    
    * Adding advanced mode
    
    * Formatting and solving conflicts
    
    * Solving small template issues
    
    * Formatting the template after generation
    
    * Formatting pallet
    
    * Updating template generation
    
    * Improving manifest test suite
    
    * Allowing storage/common_types from CLI + template improvement
    
    * Solving bug related to PR #277: Regenerating a crate with POP that's already in the workspace doesn't include it again
    
    * Finishing templates with examples; solving some small bugs produced when a pallet is generated from a workspace
    
    * Finishing templates with examples; solving some small bugs produced when a pallet is generated from a workspace
    
    * Deleting legacy file
    
    * Improving UX
    
    * Applying suggested changes
    
    * Applying suggested changes
    
    * Adding a whiteline to lib.rs.templ for formatting, not so important
    
    * Not including default config if config trait empty
    
    * Improve advanced help examples
    
    * Improve advanced help examples
    
    * Improve advanced help examples
    
    * Applying suggestions
    
    ---------
    
    Co-authored-by: Bruno Galvao <brunopgalvao@gmail.com>

--- crates/pop-parachains/src/generator/pallet.rs
+++ crates/pop-parachains/src/generator/pallet.rs
@@ -2,8 +2,17 @@
 
 use std::path::PathBuf;
 
-use crate::utils::helpers::write_to_file;
+use crate::{
+	utils::helpers::write_to_file, TemplatePalletConfigCommonTypes, TemplatePalletStorageTypes,
+};
 use askama::Template;
+
+mod filters {
+	/// This filter is used to determine if a element is present in a `Vec`
+	pub fn contains<T: PartialEq>(vec: &Vec<T>, element: T) -> ::askama::Result<bool> {
+		Ok(vec.contains(&element))
+	}
+}
 
 #[derive(Template)]
 #[template(path = "pallet/Cargo.templ", escape = "none")]
@@ -11,30 +20,104 @@ pub(crate) struct PalletCargoToml {
 	pub(crate) name: String,
 	pub(crate) authors: String,
 	pub(crate) description: String,
+	// A bool indicating if the pallet has been generated inside a workspace
 	pub(crate) pallet_in_workspace: bool,
+	// Some common types are used to couple our pallet with a well known one, then adding this type
+	// here is useful to design Cargo.toml. This pallets should be added as dev-dependencies to
+	// construct the mock runtime
+	pub(crate) pallet_common_types: Vec<TemplatePalletConfigCommonTypes>,
 }
+
+// Templates for simple mode
 #[derive(Template)]
-#[template(path = "pallet/src/benchmarking.rs.templ", escape = "none")]
-pub(crate) struct PalletBenchmarking {}
-#[derive(Template)]
-#[template(path = "pallet/src/lib.rs.templ", escape = "none")]
-pub(crate) struct PalletLib {
-	pub(crate) name: String,
-}
-#[derive(Template)]
-#[template(path = "pallet/src/mock.rs.templ", escape = "none")]
-pub(crate) struct PalletMock {
-	pub(crate) name: String,
-}
-#[derive(Template)]
-#[template(path = "pallet/src/tests.rs.templ", escape = "none")]
-pub(crate) struct PalletTests {
+#[template(path = "pallet/simple_mode/src/lib.rs.templ", escape = "none")]
+pub(crate) struct PalletSimpleLib {
 	pub(crate) name: String,
 }
 
 #[derive(Template)]
-#[template(path = "pallet/src/weights.rs.templ", escape = "none")]
+#[template(path = "pallet/simple_mode/src/tests.rs.templ", escape = "none")]
+pub(crate) struct PalletSimpleTests {
+	pub(crate) name: String,
+}
+
+#[derive(Template)]
+#[template(path = "pallet/simple_mode/src/mock.rs.templ", escape = "none")]
+pub(crate) struct PalletSimpleMock {
+	pub(crate) name: String,
+}
+
+#[derive(Template)]
+#[template(path = "pallet/simple_mode/src/benchmarking.rs.templ", escape = "none")]
+pub(crate) struct PalletSimpleBenchmarking {}
+
+#[derive(Template)]
+#[template(path = "pallet/simple_mode/src/weights.rs.templ", escape = "none")]
 pub(crate) struct PalletWeights {}
+
+// Templates for advanced mode
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/lib.rs.templ", escape = "none")]
+pub(crate) struct PalletAdvancedLib {
+	pub(crate) name: String,
+	pub(crate) pallet_default_config: bool,
+	pub(crate) pallet_common_types: Vec<TemplatePalletConfigCommonTypes>,
+	pub(crate) pallet_storage: Vec<TemplatePalletStorageTypes>,
+	pub(crate) pallet_genesis: bool,
+	pub(crate) pallet_custom_origin: bool,
+}
+
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/tests.rs.templ", escape = "none")]
+pub(crate) struct PalletAdvancedTests {}
+
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/mock.rs.templ", escape = "none")]
+pub(crate) struct PalletAdvancedMock {
+	pub(crate) name: String,
+	pub(crate) pallet_default_config: bool,
+	pub(crate) pallet_common_types: Vec<TemplatePalletConfigCommonTypes>,
+	pub(crate) pallet_custom_origin: bool,
+}
+
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/benchmarking.rs.templ", escape = "none")]
+pub(crate) struct PalletAdvancedBenchmarking {}
+
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/pallet_logic.rs.templ", escape = "none")]
+pub(crate) struct PalletLogic {
+	pub(crate) pallet_custom_origin: bool,
+}
+
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/config_preludes.rs.templ", escape = "none")]
+pub(crate) struct PalletConfigPreludes {
+	pub(crate) pallet_common_types: Vec<TemplatePalletConfigCommonTypes>,
+	pub(crate) pallet_custom_origin: bool,
+}
+
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/pallet_logic/try_state.rs.templ", escape = "none")]
+pub(crate) struct PalletTryState {}
+
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/pallet_logic/origin.rs.templ", escape = "none")]
+pub(crate) struct PalletOrigin {}
+
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/tests/utils.rs.templ", escape = "none")]
+pub(crate) struct PalletTestsUtils {
+	pub(crate) name: String,
+}
+
+#[derive(Template)]
+#[template(path = "pallet/advanced_mode/src/types.rs.templ", escape = "none")]
+pub(crate) struct PalletTypes {
+	pub(crate) pallet_common_types: Vec<TemplatePalletConfigCommonTypes>,
+	pub(crate) pallet_storage: Vec<TemplatePalletStorageTypes>,
+	pub(crate) pallet_custom_origin: bool,
+}
 
 pub trait PalletItem {
 	/// Render and Write to file, root is the path to the pallet
@@ -53,9 +136,19 @@ macro_rules! generate_pallet_item {
 	};
 }
 
-generate_pallet_item!(PalletTests, "src/tests.rs");
-generate_pallet_item!(PalletMock, "src/mock.rs");
-generate_pallet_item!(PalletLib, "src/lib.rs");
-generate_pallet_item!(PalletBenchmarking, "src/benchmarking.rs");
 generate_pallet_item!(PalletCargoToml, "Cargo.toml");
+generate_pallet_item!(PalletSimpleLib, "src/lib.rs");
+generate_pallet_item!(PalletSimpleTests, "src/tests.rs");
+generate_pallet_item!(PalletSimpleMock, "src/mock.rs");
+generate_pallet_item!(PalletSimpleBenchmarking, "src/benchmarking.rs");
 generate_pallet_item!(PalletWeights, "src/weights.rs");
+generate_pallet_item!(PalletAdvancedLib, "src/lib.rs");
+generate_pallet_item!(PalletAdvancedTests, "src/tests.rs");
+generate_pallet_item!(PalletAdvancedMock, "src/mock.rs");
+generate_pallet_item!(PalletAdvancedBenchmarking, "src/benchmarking.rs");
+generate_pallet_item!(PalletLogic, "src/pallet_logic.rs");
+generate_pallet_item!(PalletConfigPreludes, "src/config_preludes.rs");
+generate_pallet_item!(PalletTryState, "src/pallet_logic/try_state.rs");
+generate_pallet_item!(PalletOrigin, "src/pallet_logic/origin.rs");
+generate_pallet_item!(PalletTestsUtils, "src/tests/utils.rs");
+generate_pallet_item!(PalletTypes, "src/types.rs");
