8e50d0b4c4dcfea8988702762c4ae71764510e87
AlexD10S <alexfraga10@gmail.com>
2024-09-24 19:20:05 +0200

    refactor: various fixes

--- crates/pop-common/src/lib.rs
+++ crates/pop-common/src/lib.rs
@@ -40,18 +40,16 @@ pub fn target() -> Result<&'static str, Error> {
 	}
 
 	match ARCH {
-		"aarch64" => {
+		"aarch64" =>
 			return match OS {
 				"macos" => Ok("aarch64-apple-darwin"),
 				_ => Ok("aarch64-unknown-linux-gnu"),
-			}
-		},
-		"x86_64" | "x86" => {
+			},
+		"x86_64" | "x86" =>
 			return match OS {
 				"macos" => Ok("x86_64-apple-darwin"),
 				_ => Ok("x86_64-unknown-linux-gnu"),
-			}
-		},
+			},
 		&_ => {},
 	}
 	Err(Error::UnsupportedPlatform { arch: ARCH, os: OS })
