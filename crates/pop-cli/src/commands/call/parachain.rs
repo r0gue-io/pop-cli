7af045afc990586c19b45d7e4d4a6c6698d74c5a
AlexD10S <alexfraga10@gmail.com>
2024-09-27 11:44:58 +0200

    feat: add option to include params from command line

--- crates/pop-cli/src/commands/call/parachain.rs
+++ crates/pop-cli/src/commands/call/parachain.rs
@@ -3,17 +3,22 @@
 use crate::cli::{self, traits::*};
 use anyhow::{anyhow, Result};
 use clap::Args;
-use pop_common::parse_account;
 use pop_parachains::{
-	prepare_extrinsic, set_up_api, submit_extrinsic, supported_extrinsics, Extrinsic, OnlineClient,
-	SubstrateConfig, Value,
+	construct_extrinsic, encode_call_data, set_up_api, sign_and_submit_extrinsic,
+	supported_extrinsics, DynamicPayload, Extrinsic, OnlineClient, SubstrateConfig,
 };
 
 #[derive(Args, Clone)]
 pub struct CallParachainCommand {
-	/// The signed extrinsic to submit.
+	/// The name of the pallet to call.
+	#[clap(long, short)]
+	pallet: Option<String>,
+	/// The name of the extrinsic to submit.
 	#[clap(long, short)]
 	extrinsic: Option<String>,
+	/// The constructor arguments, encoded as strings.
+	#[clap(long, num_args = 0..)]
+	args: Vec<String>,
 	/// Websocket endpoint of a node.
 	#[clap(name = "url", long, value_parser, default_value = "ws://127.0.0.1:9944")]
 	url: String,
@@ -30,8 +35,9 @@ impl CallParachainCommand {
 	/// Executes the command.
 	pub(crate) async fn execute(mut self) -> Result<()> {
 		let (api, url) = self.set_up_api(&mut cli::Cli).await?;
-		let call_config = if self.extrinsic.is_none() {
-			match guide_user_to_call_chain(&api, url, &mut cli::Cli).await {
+		// TODO: Option to send call_data, sign it and send it.
+		let mut call_config = if self.pallet.is_none() && self.extrinsic.is_none() {
+			match guide_user_to_call_chain(&api, "", &url, &mut cli::Cli).await {
 				Ok(call_config) => call_config,
 				Err(e) => {
 					display_message(&format!("{}", e), false, &mut cli::Cli)?;
@@ -41,7 +47,13 @@ impl CallParachainCommand {
 		} else {
 			self.clone()
 		};
-		execute_extrinsic(api, call_config, self.extrinsic.is_none(), &mut cli::Cli).await?;
+		prepare_and_submit_extrinsic(
+			api,
+			&mut call_config,
+			self.pallet.is_none() && self.extrinsic.is_none(),
+			&mut cli::Cli,
+		)
+		.await?;
 		Ok(())
 	}
 	/// Prompt the user for the chain to use if not indicated and fetch the metadata.
@@ -50,7 +62,7 @@ impl CallParachainCommand {
 		cli: &mut impl cli::traits::Cli,
 	) -> anyhow::Result<(OnlineClient<SubstrateConfig>, String)> {
 		cli.intro("Call a parachain")?;
-		let url: String = if self.extrinsic.is_none() {
+		let url: String = if self.pallet.is_none() && self.extrinsic.is_none() {
 			// Prompt for contract location.
 			cli.input("Which chain would you like to interact with?")
 				.placeholder("wss://rpc1.paseo.popnetwork.xyz")
@@ -65,8 +77,14 @@ impl CallParachainCommand {
 
 	fn display(&self) -> String {
 		let mut full_message = "pop call parachain".to_string();
+		if let Some(pallet) = &self.pallet {
+			full_message.push_str(&format!(" --pallet {}", pallet));
+		}
 		if let Some(extrinsic) = &self.extrinsic {
 			full_message.push_str(&format!(" --extrinsic {}", extrinsic));
+		}
+		if self.args.len() > 0 {
+			full_message.push_str(&format!(" --args {}", self.args.join(" ")));
 		}
 		full_message.push_str(&format!(" --url {}", self.url));
 		if !self.suri.is_empty() {
@@ -79,11 +97,12 @@ impl CallParachainCommand {
 /// Guide the user to call the contract.
 async fn guide_user_to_call_chain(
 	api: &OnlineClient<SubstrateConfig>,
-	url: String,
+	suri: &str,
+	url: &str,
 	cli: &mut impl cli::traits::Cli,
 ) -> anyhow::Result<CallParachainCommand> {
 	let extrinsic = {
-		let mut prompt_extrinsic = cli.select("Select the extrinsic to call:");
+		let mut prompt_extrinsic = cli.select("What would you like to do on Polkadot?");
 		//for extrinsic in pallet.extrinsics() {
 		for extrinsic in supported_extrinsics(api) {
 			prompt_extrinsic = prompt_extrinsic.item(
@@ -95,62 +114,102 @@ async fn guide_user_to_call_chain(
 		prompt_extrinsic.interact()?
 	};
 	let args = prompt_arguments(&extrinsic, cli)?;
-	let suri = cli::Cli
-		.input("Who is going to sign the extrinsic:")
-		.placeholder("//Alice")
-		.default_input("//Alice")
-		.interact()?;
 
-	match prepare_extrinsic(api, extrinsic.pallet(), extrinsic.extrinsic_name(), args, &suri).await
-	{
-		Ok(encoded_call_data) =>
-			Ok(CallParachainCommand { extrinsic: Some(encoded_call_data), url, suri }),
-		Err(e) => Err(anyhow!(format!("{}", e.to_string()))),
-	}
+	Ok(CallParachainCommand {
+		pallet: Some(extrinsic.pallet().to_string()),
+		extrinsic: Some(extrinsic.extrinsic_name().to_string()),
+		args,
+		url: url.to_string(),
+		suri: suri.to_string(),
+	})
 }
 
-/// Executes the extrinsic or query.
-async fn execute_extrinsic(
+/// Prepares the extrinsic or query.
+async fn prepare_and_submit_extrinsic(
 	api: OnlineClient<SubstrateConfig>,
-	call_config: CallParachainCommand,
+	call_config: &mut CallParachainCommand,
 	prompt_to_repeat_call: bool,
 	cli: &mut impl cli::traits::Cli,
 ) -> Result<()> {
-	cli.info(call_config.display())?;
-	let extrinsic = call_config
+	let extrinsic: String = call_config
 		.extrinsic
+		.clone()
 		.expect("extrinsic can not be none as fallback above is interactive input; qed");
-	if !cli.confirm("Do you want to submit the call?").interact()? {
-		display_message(&format!("Extrinsic: {} not submitted", extrinsic), true, cli)?;
+	let pallet = match call_config.pallet.clone() {
+		Some(m) => m,
+		None => {
+			return Err(anyhow!("Please specify the pallet to call."));
+		},
+	};
+	let tx = match construct_extrinsic(&pallet, &extrinsic, &call_config.args) {
+		Ok(tx) => tx,
+		Err(e) => {
+			display_message(
+				&format!("Error parsing the arguments: {}", e.to_string()),
+				false,
+				&mut cli::Cli,
+			)?;
+			return Ok(());
+		},
+	};
+	cli.info(format!("Call data {}", encode_call_data(&api, &tx)?))?;
+	if call_config.suri.is_empty() {
+		call_config.suri = cli::Cli
+			.input("Who is going to sign the extrinsic:")
+			.placeholder("//Alice")
+			.default_input("//Alice")
+			.interact()?;
+	}
+	cli.info(call_config.display())?;
+	if !cli.confirm("Do you want to submit the call?").initial_value(true).interact()? {
+		display_message(
+			&format!("Extrinsic: {} not submitted, user cancel the operation", extrinsic),
+			false,
+			cli,
+		)?;
 		return Ok(());
 	}
+	send_extrinsic(api, tx, &call_config.url, &call_config.suri, prompt_to_repeat_call, cli)
+		.await?;
+
+	Ok(())
+}
+
+async fn send_extrinsic(
+	api: OnlineClient<SubstrateConfig>,
+	tx: DynamicPayload,
+	url: &str,
+	suri: &str,
+	prompt_to_repeat_call: bool,
+	cli: &mut impl cli::traits::Cli,
+) -> Result<()> {
 	let spinner = cliclack::spinner();
-	spinner.start("Submitting the extrinsic...");
-	match submit_extrinsic(api.clone(), extrinsic).await {
-		Ok(result) => {
-			console::Term::stderr().clear_last_lines(1)?;
-			display_message(
-				&format!("Extrinsic submitted successfully with hash: {:?}", result),
-				true,
-				cli,
-			)?;
-		},
-		Err(e) => {
-			console::Term::stderr().clear_last_lines(1)?;
-			display_message(&format!("{}", e), false, cli)?;
-		},
-	}
-	// Repeat call.
-	if prompt_to_repeat_call {
-		let another_call: bool = cli
-			.confirm("Do you want to do another call to the same chain?")
-			.initial_value(false)
-			.interact()?;
-		if another_call {
-			// Remove only the prompt asking for another call.
-			console::Term::stderr().clear_last_lines(2)?;
-			let new_call_config = guide_user_to_call_chain(&api, call_config.url, cli).await?;
-			Box::pin(execute_extrinsic(api, new_call_config, prompt_to_repeat_call, cli)).await?;
+	spinner.start("Signing and submitting the extrinsic, please wait...");
+	let result = sign_and_submit_extrinsic(api.clone(), tx, suri).await;
+	if let Err(e) = result {
+		console::Term::stderr().clear_last_lines(1)?;
+		display_message(&format!("{}", e), false, cli)?;
+	} else {
+		console::Term::stderr().clear_last_lines(1)?;
+		display_message(&format!("Extrinsic submitted with hash: {:?}", result?), true, cli)?;
+		// Repeat call.
+		if prompt_to_repeat_call {
+			let another_call: bool = cli
+				.confirm("Do you want to do another call to the same chain?")
+				.initial_value(false)
+				.interact()?;
+			if another_call {
+				// Remove only the prompt asking for another call.
+				console::Term::stderr().clear_last_lines(2)?;
+				let mut new_call_config = guide_user_to_call_chain(&api, suri, url, cli).await?;
+				Box::pin(prepare_and_submit_extrinsic(
+					api,
+					&mut new_call_config,
+					prompt_to_repeat_call,
+					cli,
+				))
+				.await?;
+			}
 		}
 	}
 	Ok(())
@@ -165,14 +224,13 @@ fn display_message(message: &str, success: bool, cli: &mut impl cli::traits::Cli
 	Ok(())
 }
 // Prompt the user to select an operation.
-fn prompt_arguments(extrinsic: &Extrinsic, cli: &mut impl cli::traits::Cli) -> Result<Vec<Value>> {
-	let mut args: Vec<Value> = Vec::new();
+fn prompt_arguments(extrinsic: &Extrinsic, cli: &mut impl cli::traits::Cli) -> Result<Vec<String>> {
+	let mut args: Vec<String> = Vec::new();
 	match extrinsic {
 		Extrinsic::CreateAsset => {
 			args.push(prompt_for_numeric_value("Enter the Asset ID", cli)?);
 			args.push(prompt_for_account("Enter the Admin Address", cli)?);
 			args.push(prompt_for_numeric_value("Enter the Minimum Balance", cli)?);
-			// TODO: ADD METADATA
 		},
 		Extrinsic::MintAsset => {
 			args.push(prompt_for_numeric_value("Enter the Asset ID", cli)?);
@@ -181,13 +239,13 @@ fn prompt_arguments(extrinsic: &Extrinsic, cli: &mut impl cli::traits::Cli) -> R
 		},
 		Extrinsic::CreateCollection => {
 			args.push(prompt_for_account("Enter the Admin Address", cli)?);
-			args.push(prompt_for_collection_config(cli)?);
+			args.extend(prompt_for_collection_config(cli)?);
 		},
 		Extrinsic::MintNFT => {
 			args.push(prompt_for_numeric_value("Enter the Collection ID", cli)?);
 			args.push(prompt_for_numeric_value("Enter the Item ID", cli)?);
 			args.push(prompt_for_account("Enter the Beneficiary Address", cli)?);
-			args.push(prompt_for_witness_data(cli)?);
+			args.extend(prompt_for_witness_data(cli)?);
 		},
 		Extrinsic::Transfer => {
 			args.push(prompt_for_account("Enter the Destination Address", cli)?);
@@ -196,7 +254,7 @@ fn prompt_arguments(extrinsic: &Extrinsic, cli: &mut impl cli::traits::Cli) -> R
 	}
 	Ok(args)
 }
-fn prompt_for_numeric_value(message: &str, cli: &mut impl cli::traits::Cli) -> Result<Value> {
+fn prompt_for_numeric_value(message: &str, cli: &mut impl cli::traits::Cli) -> Result<String> {
 	let id = cli
 		.input(message)
 		.placeholder("0")
@@ -207,88 +265,85 @@ fn prompt_for_numeric_value(message: &str, cli: &mut impl cli::traits::Cli) -> R
 		})
 		.required(true)
 		.interact()?;
-	Ok(Value::u128(id.parse::<u128>()?))
+	Ok(id)
 }
-fn prompt_for_account(message: &str, cli: &mut impl cli::traits::Cli) -> Result<Value> {
+fn prompt_for_account(message: &str, cli: &mut impl cli::traits::Cli) -> Result<String> {
 	let account: String = cli
 		.input(message)
 		.placeholder("e.g. 5DYs7UGBm2LuX4ryvyqfksozNAW5V47tPbGiVgnjYWCZ29bt")
 		.required(true)
 		.interact()?;
-	let account_id = parse_account(&account)?;
+	//let account_id = parse_account(&account)?;
 	// TODO: Support other Adresses? Let the user pick Id, Address, or Index
-	Ok(Value::unnamed_variant("Id", vec![Value::from_bytes(account_id)]))
+	Ok(account)
 }
 fn prompt_for_numeric_optional_value(
 	message: &str,
 	cli: &mut impl cli::traits::Cli,
-) -> Result<Value> {
+) -> Result<String> {
 	let value = cli
 		.input(message)
 		.placeholder("0 or (empty for None)")
 		.validate(|input: &String| match input.parse::<u128>() {
 			Ok(_) => Ok(()),
-			Err(_) =>
+			Err(_) => {
 				if input.is_empty() || input == "None" {
 					Ok(())
 				} else {
 					Err("Invalid value.")
-				},
+				}
+			},
 		})
 		.required(false)
 		.interact()?;
 	if value.is_empty() || value == "None" {
-		Ok(Value::unnamed_variant("None", vec![]))
+		Ok("None".to_string())
+	//Ok(Value::unnamed_variant("None", vec![]))
 	} else {
-		Ok(Value::unnamed_variant("Some", vec![Value::u128(value.parse::<u128>()?)]))
+		Ok(value)
+		//Ok(Value::unnamed_variant("Some", vec![Value::u128(value.parse::<u128>()?)]))
 	}
 }
 fn prompt_for_variant_value(
 	message: &str,
 	default_value: &str,
 	cli: &mut impl cli::traits::Cli,
-) -> Result<Value> {
+) -> Result<String> {
 	let mint_type: String = cli
 		.input(message)
 		.placeholder(&format!("e.g. {}", default_value))
 		.default_input(default_value)
 		.required(true)
 		.interact()?;
-	Ok(Value::unnamed_variant(mint_type, vec![]))
+	Ok(mint_type)
 }
-fn prompt_for_collection_config(cli: &mut impl cli::traits::Cli) -> Result<Value> {
+fn prompt_for_collection_config(cli: &mut impl cli::traits::Cli) -> Result<Vec<String>> {
+	let mut args: Vec<String> = Vec::new();
 	cli.info("Enter the Pallet NFT Collection Config:")?;
-	let settings = prompt_for_numeric_value("Collection's Settings", cli)?;
-	let max_supply = prompt_for_numeric_optional_value("Collection's Max Supply", cli)?;
+	args.push(prompt_for_numeric_value("Collection's Settings", cli)?);
+	args.push(prompt_for_numeric_optional_value("Collection's Max Supply", cli)?);
 	cli.info("Enter the Mint Settings:")?;
-	let mint_type = prompt_for_variant_value("Who can mint?", "Issuer", cli)?;
-	let price_per_mint = prompt_for_numeric_optional_value("Price per mint", cli)?;
-	let start_block = prompt_for_numeric_optional_value("When the mint starts", cli)?;
-	let end_block = prompt_for_numeric_optional_value("When the mint ends", cli)?;
-	let default_item_settings = prompt_for_numeric_value("Default Item Settings", cli)?;
-	// mint settings
-	let mint_settings = Value::unnamed_composite(vec![
-		mint_type,
-		price_per_mint,
-		start_block,
-		end_block,
-		default_item_settings,
-	]);
-	let config_collection = Value::unnamed_composite(vec![settings, max_supply, mint_settings]);
-
-	Ok(config_collection)
+	args.push(prompt_for_variant_value("Who can mint?", "Issuer", cli)?);
+	args.push(prompt_for_numeric_optional_value("Price per mint", cli)?);
+	args.push(prompt_for_numeric_optional_value("When the mint starts", cli)?);
+	args.push(prompt_for_numeric_optional_value("When the mint ends", cli)?);
+	args.push(prompt_for_numeric_value("Default Item Settings", cli)?);
+	Ok(args)
 }
-fn prompt_for_witness_data(cli: &mut impl cli::traits::Cli) -> Result<Value> {
+fn prompt_for_witness_data(cli: &mut impl cli::traits::Cli) -> Result<Vec<String>> {
+	let mut args: Vec<String> = Vec::new();
 	if cli
 		.confirm("Do you want to enter witness data for mint")
 		.initial_value(false)
 		.interact()?
 	{
-		let owned_item =
-			prompt_for_numeric_optional_value("Id of the item in a required collection:", cli)?;
-		let mint_price = prompt_for_numeric_optional_value("Mint price:", cli)?;
-		Ok(Value::unnamed_variant("Some".to_string(), vec![owned_item, mint_price]))
+		args.push(prompt_for_numeric_optional_value(
+			"Id of the item in a required collection:",
+			cli,
+		)?);
+		args.push(prompt_for_numeric_optional_value("Mint price:", cli)?);
 	} else {
-		Ok(Value::unnamed_variant("None", vec![]))
+		args.push("None".to_string());
 	}
+	Ok(args)
 }
--- crates/pop-parachains/src/call.rs
+++ crates/pop-parachains/src/call.rs
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0
 
 use crate::errors::Error;
-use pop_common::create_signer;
+use pop_common::{create_signer, parse_account};
 use strum::{EnumMessage as _, EnumProperty as _, VariantArray as _};
 use strum_macros::{AsRefStr, Display, EnumMessage, EnumProperty, EnumString, VariantArray};
 use subxt::{
-	config::DefaultExtrinsicParamsBuilder, dynamic::Value, tx::SubmittableExtrinsic, OnlineClient,
-	SubstrateConfig,
+	dynamic::Value,
+	tx::{DynamicPayload, Payload},
+	OnlineClient, SubstrateConfig,
 };
+
+#[derive(AsRefStr, Clone, Debug, Display, EnumMessage, EnumString, Eq, PartialEq, VariantArray)]
+pub enum Pallet {
+	#[strum(serialize = "Assets")]
+	Assets,
+	#[strum(serialize = "Balances")]
+	Balances,
+	#[strum(serialize = "Nfts")]
+	Nfts,
+}
 
 #[derive(
 	AsRefStr,
@@ -15,22 +26,22 @@ use subxt::{
 	Debug,
 	Display,
 	EnumMessage,
-	EnumProperty,
 	EnumString,
+	EnumProperty,
 	Eq,
 	PartialEq,
 	VariantArray,
 )]
 pub enum Extrinsic {
 	#[strum(
-		serialize = "create_asset",
+		serialize = "create",
 		message = "create",
 		detailed_message = "Create an Asset",
 		props(Pallet = "Assets")
 	)]
 	CreateAsset,
 	#[strum(
-		serialize = "mint_asset",
+		serialize = "mint",
 		message = "mint",
 		detailed_message = "Mint an Asset",
 		props(Pallet = "Assets")
@@ -44,7 +55,7 @@ pub enum Extrinsic {
 	)]
 	CreateCollection,
 	#[strum(
-		serialize = "mint",
+		serialize = "mint_nft",
 		message = "mint",
 		detailed_message = "Mint a NFT",
 		props(Pallet = "Nfts")
@@ -73,6 +84,7 @@ impl Extrinsic {
 		self.get_str("Pallet").unwrap_or_default()
 	}
 }
+
 pub fn supported_extrinsics(api: &OnlineClient<SubstrateConfig>) -> Vec<&Extrinsic> {
 	Extrinsic::VARIANTS
 		.iter()
@@ -80,44 +92,183 @@ pub fn supported_extrinsics(api: &OnlineClient<SubstrateConfig>) -> Vec<&Extrins
 		.collect()
 }
 
+pub fn parse_args(
+	pallet_name: &str,
+	extrinsic_name: &str,
+	raw_args: &Vec<String>,
+) -> Result<Vec<Value>, Error> {
+	let mut args: Vec<Value> = Vec::new();
+	let extrinsic = Extrinsic::VARIANTS
+		.iter()
+		.find(|t| t.pallet() == pallet_name && t.extrinsic_name() == extrinsic_name)
+		.ok_or(Error::ExtrinsicNotSupported(extrinsic_name.to_string()))?;
+	match extrinsic {
+		Extrinsic::CreateAsset => {
+			args.push(Value::u128(
+				raw_args[0].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+			));
+			args.push(Value::unnamed_variant(
+				"Id",
+				vec![Value::from_bytes(parse_account(&raw_args[1])?)],
+			));
+			args.push(Value::u128(
+				raw_args[2].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+			));
+		},
+		Extrinsic::MintAsset => {
+			args.push(Value::u128(
+				raw_args[0].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+			));
+			args.push(Value::unnamed_variant(
+				"Id",
+				vec![Value::from_bytes(parse_account(&raw_args[1])?)],
+			));
+			args.push(Value::u128(
+				raw_args[2].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+			));
+		},
+		Extrinsic::CreateCollection => {
+			args.push(Value::unnamed_variant(
+				"Id",
+				vec![Value::from_bytes(parse_account(&raw_args[0])?)],
+			));
+			let mint_settings = Value::unnamed_composite(vec![
+				Value::unnamed_variant(&raw_args[3], vec![]),
+				if raw_args[4] == "None" {
+					Value::unnamed_variant("None", vec![])
+				} else {
+					Value::unnamed_variant(
+						"Some",
+						vec![Value::u128(
+							raw_args[4].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+						)],
+					)
+				},
+				if raw_args[5] == "None" {
+					Value::unnamed_variant("None", vec![])
+				} else {
+					Value::unnamed_variant(
+						"Some",
+						vec![Value::u128(
+							raw_args[5].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+						)],
+					)
+				},
+				if raw_args[6] == "None" {
+					Value::unnamed_variant("None", vec![])
+				} else {
+					Value::unnamed_variant(
+						"Some",
+						vec![Value::u128(
+							raw_args[6].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+						)],
+					)
+				},
+				Value::u128(raw_args[7].parse::<u128>().map_err(|_| Error::ParsingArgsError)?),
+			]);
+			let max_supply = if raw_args[2] == "None" {
+				Value::unnamed_variant("None", vec![])
+			} else {
+				Value::unnamed_variant(
+					"Some",
+					vec![Value::u128(
+						raw_args[2].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+					)],
+				)
+			};
+			args.push(Value::unnamed_composite(vec![
+				Value::u128(raw_args[1].parse::<u128>().map_err(|_| Error::ParsingArgsError)?),
+				max_supply,
+				mint_settings,
+			]))
+		},
+		Extrinsic::MintNFT => {
+			args.push(Value::u128(
+				raw_args[0].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+			));
+			args.push(Value::u128(
+				raw_args[1].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+			));
+			args.push(Value::unnamed_variant(
+				"Id",
+				vec![Value::from_bytes(parse_account(&raw_args[2])?)],
+			));
+			if raw_args[3] == "None" && raw_args.len() == 4 {
+				args.push(Value::unnamed_variant("None", vec![]));
+			} else {
+				let owned_item = if raw_args[3] == "None" {
+					Value::unnamed_variant("None", vec![])
+				} else {
+					Value::unnamed_variant(
+						"Some",
+						vec![Value::u128(
+							raw_args[3].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+						)],
+					)
+				};
+				let mint_price = if raw_args[4] == "None" {
+					Value::unnamed_variant("None", vec![])
+				} else {
+					Value::unnamed_variant(
+						"Some",
+						vec![Value::u128(
+							raw_args[4].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+						)],
+					)
+				};
+				args.push(Value::unnamed_variant("Some".to_string(), vec![owned_item, mint_price]));
+			}
+		},
+		Extrinsic::Transfer => {
+			args.push(Value::unnamed_variant(
+				"Id",
+				vec![Value::from_bytes(parse_account(&raw_args[0])?)],
+			));
+			args.push(Value::u128(
+				raw_args[1].parse::<u128>().map_err(|_| Error::ParsingArgsError)?,
+			));
+		},
+	}
+	Ok(args)
+}
+
 pub async fn set_up_api(url: &str) -> Result<OnlineClient<SubstrateConfig>, Error> {
 	let api = OnlineClient::<SubstrateConfig>::from_url(url).await?;
 	Ok(api)
 }
 
-pub async fn prepare_extrinsic(
-	api: &OnlineClient<SubstrateConfig>,
+pub fn construct_extrinsic(
 	pallet_name: &str,
-	entry_name: &str,
-	args_value: Vec<Value>,
+	extrinsic_name: &str,
+	args: &Vec<String>,
+) -> Result<DynamicPayload, Error> {
+	let parsed_args: Vec<Value> = parse_args(pallet_name, extrinsic_name, &args)?;
+	Ok(subxt::dynamic::tx(pallet_name, extrinsic_name, parsed_args))
+}
+
+pub async fn sign_and_submit_extrinsic(
+	api: OnlineClient<SubstrateConfig>,
+	tx: DynamicPayload,
 	suri: &str,
 ) -> Result<String, Error> {
 	let signer = create_signer(suri)?;
-	let tx = subxt::dynamic::tx(pallet_name, entry_name, args_value);
-	let signed_extrinsic: SubmittableExtrinsic<SubstrateConfig, OnlineClient<SubstrateConfig>> =
-		api.tx()
-			.create_signed(&tx, &signer, DefaultExtrinsicParamsBuilder::new().build())
-			.await?;
-	Ok(encode_extrinsic(signed_extrinsic.encoded().to_vec()))
-}
-
-pub async fn submit_extrinsic(
-	api: OnlineClient<SubstrateConfig>,
-	encoded_extrinsic: String,
-) -> Result<String, Error> {
-	let extrinsic = decode_extrinsic(encoded_extrinsic)?;
-	let signed_extrinsic: SubmittableExtrinsic<SubstrateConfig, OnlineClient<SubstrateConfig>> =
-		SubmittableExtrinsic::from_bytes(api, extrinsic);
-	let result = signed_extrinsic.submit_and_watch().await?;
+	let result = api
+		.tx()
+		.sign_and_submit_then_watch_default(&tx, &signer)
+		.await?
+		.wait_for_finalized()
+		.await?
+		.wait_for_success()
+		.await?;
 	Ok(format!("{:?}", result.extrinsic_hash()))
 }
 
-fn encode_extrinsic(encoded_call_data: Vec<u8>) -> String {
-	format!("0x{}", hex::encode(encoded_call_data))
-}
-fn decode_extrinsic(encoded_call_data: String) -> Result<Vec<u8>, Error> {
-	let hex_data = encoded_call_data.trim_start_matches("0x");
-	Ok(hex::decode(hex_data)?)
+pub fn encode_call_data(
+	api: &OnlineClient<SubstrateConfig>,
+	tx: &DynamicPayload,
+) -> Result<String, Error> {
+	let call_data = tx.encode_call_data(&api.metadata())?;
+	Ok(format!("0x{}", hex::encode(call_data)))
 }
 
 fn extrinsic_is_supported(
@@ -142,12 +293,30 @@ fn extrinsic_is_supported(
 mod tests {
 	use super::*;
 	use anyhow::Result;
+	use subxt::tx::Payload;
 
 	#[tokio::test]
 	async fn extrinsic_is_supported_works() -> Result<()> {
 		let api = set_up_api("wss://rpc1.paseo.popnetwork.xyz").await?;
 		assert!(extrinsic_is_supported(&api, "Nfts", "mint"));
 		assert!(!extrinsic_is_supported(&api, "Nfts", "mint_no_exist"));
+		Ok(())
+	}
+
+	#[tokio::test]
+	async fn encode_call_data_works() -> Result<()> {
+		let api = set_up_api("wss://rpc1.paseo.popnetwork.xyz").await?;
+		let tx = construct_extrinsic(
+			"Assets",
+			"create",
+			&vec![
+				"1000".to_string(),
+				"15oF4uVJwmo4TdGW7VfQxNLavjCXviqxT9S1MgbjMNHr6Sp5".to_string(),
+				"1000000".to_string(),
+			],
+		)?;
+		let call_data = tx.encode_call_data(&api.metadata())?;
+		assert_eq!("0x3400419c00d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d40420f00000000000000000000000000", encode_call_data(call_data));
 		Ok(())
 	}
 }
