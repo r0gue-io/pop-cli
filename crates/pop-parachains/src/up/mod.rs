063b5986b67c140f59941a8cd7b03482b61c046e
Frank Bell <frank@r0gue.io>
2024-10-10 15:24:06 +0100

    chore: update zombienet-sdk

--- crates/pop-parachains/src/up/mod.rs
+++ crates/pop-parachains/src/up/mod.rs
@@ -16,8 +16,7 @@ use std::{
 use symlink::{remove_symlink_file, symlink_file};
 use tempfile::{Builder, NamedTempFile};
 use toml_edit::{value, ArrayOfTables, DocumentMut, Formatted, Item, Table, Value};
-use zombienet_sdk::{Network, NetworkConfig, NetworkConfigExt};
-use zombienet_support::fs::local::LocalFileSystem;
+use zombienet_sdk::{LocalFileSystem, Network, NetworkConfig, NetworkConfigExt};
 
 mod chain_specs;
 mod parachains;
@@ -279,10 +278,10 @@ impl Zombienet {
 		Ok(relay::default(version, runtime_version, chain, cache).await?)
 	}
 
-    /// The name of the relay chain.
-    pub fn relay_chain(&self) -> &str {
-        &self.relay_chain.chain
-    }
+	/// The name of the relay chain.
+	pub fn relay_chain(&self) -> &str {
+		&self.relay_chain.chain
+	}
 
 	/// Whether any HRMP channels are to be pre-opened.
 	pub fn hrmp_channels(&self) -> bool {
