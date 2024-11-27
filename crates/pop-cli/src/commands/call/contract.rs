ddba2363f13a233798547ecd834b7ea4f0885184
AlexD10S <alexfraga10@gmail.com>
2024-11-27 23:09:45 +0100

    fix: clippy

--- crates/pop-cli/src/commands/call/contract.rs
+++ crates/pop-cli/src/commands/call/contract.rs
@@ -199,7 +199,7 @@ impl CallContractCommand {
 		};
 
 		// Resolve contract address.
-		if let None = self.contract {
+		if self.contract.is_none() {
 			// Prompt for contract address.
 			let contract_address: String = cli
 				.input("Paste the on-chain contract address:")
