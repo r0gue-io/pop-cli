0344ea256edcc0b06db51327ccd57f76fe76dfd3
AlexD10S <alexfraga10@gmail.com>
2024-11-28 12:34:02 +0100

    test: pop-common unit tests

--- crates/pop-common/src/metadata.rs
+++ crates/pop-common/src/metadata.rs
@@ -30,7 +30,6 @@ pub fn format_type(ty: &Type<PortableForm>, registry: &PortableRegistry) -> Stri
 			.collect();
 		name = format!("{name}<{}>", params.join(","));
 	}
-
 	name = format!(
 		"{name}{}",
 		match &ty.type_def {
@@ -154,4 +153,65 @@ pub fn format_type(ty: &Type<PortableForm>, registry: &PortableRegistry) -> Stri
 	);
 
 	name
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	use anyhow::Result;
+	use subxt::{OnlineClient, SubstrateConfig};
+
+	#[tokio::test]
+	async fn format_type_works() -> Result<()> {
+		let api =
+			OnlineClient::<SubstrateConfig>::from_url("wss://rpc1.paseo.popnetwork.xyz").await?;
+		let metadata = api.metadata();
+		let registry = metadata.types();
+		// Extrinsic Nfts::mint to test the majority of expresions.
+		let mut extrinsic =
+			metadata.pallet_by_name("Nfts").unwrap().call_variant_by_name("mint").unwrap();
+		let mut types_formatted = Vec::new();
+		for field in &extrinsic.fields {
+			let type_info = registry.resolve(field.ty.id).unwrap();
+			types_formatted.push(format_type(&type_info, &registry));
+		}
+		assert_eq!(types_formatted.len(), 4);
+		assert_eq!(types_formatted[0], "u32"); // collection
+		assert_eq!(types_formatted[1], "u32"); // item
+		assert_eq!(types_formatted[2], "MultiAddress<AccountId32 ([u8;32]),()>: Id(AccountId32 ([u8;32])), Index(Compact<()>), Raw([u8]), Address32([u8;32]), Address20([u8;20])"); // mint_to
+		assert_eq!(types_formatted[3], "Option<MintWitness<u32,u128> { owned_item: Option<ItemId>, mint_price: Option<Balance> }>: None, Some(MintWitness<u32,u128> { owned_item: Option<ItemId>, mint_price: Option<Balance> })"); // witness_data
+
+		// Extrinsic Sytem::remark to testing sequences.
+		extrinsic = metadata
+			.pallet_by_name("System")
+			.unwrap()
+			.call_variant_by_name("remark")
+			.unwrap();
+		types_formatted.clear();
+		for field in &extrinsic.fields {
+			let type_info = registry.resolve(field.ty.id).unwrap();
+			types_formatted.push(format_type(&type_info, &registry));
+		}
+		assert_eq!(types_formatted.len(), 1);
+		assert_eq!(types_formatted[0], "[u8]"); // remark
+
+		// Extrinsic Scheduler::set_retry to test tuples.
+		extrinsic = metadata
+			.pallet_by_name("Scheduler")
+			.unwrap()
+			.call_variant_by_name("set_retry")
+			.unwrap();
+		types_formatted.clear();
+		for field in &extrinsic.fields {
+			let type_info = registry.resolve(field.ty.id).unwrap();
+			types_formatted.push(format_type(&type_info, &registry));
+		}
+		assert_eq!(types_formatted.len(), 3);
+		assert_eq!(types_formatted[0], "(u32,u32)"); // task
+		assert_eq!(types_formatted[1], "u8"); // retries
+		assert_eq!(types_formatted[2], "u32"); // period
+
+		Ok(())
+	}
 }
