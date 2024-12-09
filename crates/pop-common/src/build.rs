7c714388f2d3b0c75995b138ff2eb8cdd4d4df56
Frank Bell <frank@r0gue.io>
2024-12-09 22:37:16 +0000

    refactor: add missing license headers

--- crates/pop-common/src/build.rs
+++ crates/pop-common/src/build.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0
+
 use std::{
 	fmt,
 	path::{Path, PathBuf},
--- crates/pop-common/src/lib.rs
+++ crates/pop-common/src/lib.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pub mod build;
 pub mod errors;
 pub mod git;
