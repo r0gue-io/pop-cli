b1e5561cfe9b8bb653ad575b3f3130d5a1b9ec98
AlexD10S <alexfraga10@gmail.com>
2024-12-12 20:37:48 +0100

    fix: rebase

--- crates/pop-cli/src/commands/up/contract.rs
+++ crates/pop-cli/src/commands/up/contract.rs
@@ -7,7 +7,6 @@ use crate::{
 		wallet::wait_for_signature,
 	},
 	style::style,
-	wallet_integration::{FrontendFromString, TransactionData, WalletIntegrationManager},
 };
 use clap::Args;
 use cliclack::{confirm, log, log::error, spinner, ProgressBar};
@@ -231,7 +230,7 @@ impl UpContractCommand {
 				}
 			} else {
 				Cli.outro_cancel("Signed payload doesn't exist.")?;
-				Self::terminate_node(process)?;
+				terminate_node(process)?;
 				return Ok(());
 			}
 
