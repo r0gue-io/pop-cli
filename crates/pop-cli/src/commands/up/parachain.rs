f9dffc6c9ab6319534947c4e192cc032c0ea09d6
AlexD10S <alexfraga10@gmail.com>
2025-02-10 17:31:20 +0100

    feat: build specs in pop up

--- crates/pop-cli/src/commands/up/parachain.rs
+++ crates/pop-cli/src/commands/up/parachain.rs
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 
-use crate::cli;
+use crate::{
+	build::spec::BuildSpecCommand,
+	cli::{self},
+};
 use clap::Args;
 use std::path::PathBuf;
 
@@ -12,12 +15,42 @@ pub struct UpParachainCommand {
 	/// Path to the contract build directory.
 	#[clap(skip)]
 	pub(crate) path: Option<PathBuf>,
+	/// Parachain ID to be used when generating the chain spec files.
+	#[arg(short, long)]
+	pub(crate) id: Option<u32>,
+	/// Path to the genesis state file.
+	#[arg(short = 'G', long = "genesis-state")]
+	pub(crate) genesis_state: Option<PathBuf>,
+	/// Path to the genesis code file.
+	#[arg(short = 'C', long = "genesis-code")]
+	pub(crate) genesis_code: Option<PathBuf>,
 }
 
 impl UpParachainCommand {
 	/// Executes the command.
 	pub(crate) async fn execute(self, cli: &mut impl cli::traits::Cli) -> anyhow::Result<()> {
 		cli.intro("Deploy a parachain")?;
+		let para_id = self.id.unwrap_or(reserve_para_id(cli)?);
+		let (genesis_state, genesis_code) =
+			match (self.genesis_state.clone(), self.genesis_code.clone()) {
+				(Some(state), Some(code)) => (state, code),
+				_ => generate_spec_files(para_id, cli).await?,
+			};
+		cli.outro("Parachain deployment complete.")?;
 		Ok(())
 	}
+}
+
+fn reserve_para_id(cli: &mut impl cli::traits::Cli) -> anyhow::Result<u32> {
+	Ok(2000)
+}
+
+async fn generate_spec_files(
+	id: u32,
+	cli: &mut impl cli::traits::Cli,
+) -> anyhow::Result<(PathBuf, PathBuf)> {
+	let build_spec = BuildSpecCommand { id: Some(id), ..Default::default() }
+		.configure_build_spec(cli)
+		.await?;
+	build_spec.generate_genesis_artifacts(cli)
 }
