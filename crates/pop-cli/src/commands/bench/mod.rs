4c2b2deaf967e156dade6c66580a1287261e646c
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-02-11 15:28:47 +0700

    feat: add benchmark runtime wasm test

--- crates/pop-cli/src/commands/bench/mod.rs
+++ crates/pop-cli/src/commands/bench/mod.rs
@@ -18,7 +18,7 @@ pub struct BenchmarkArgs {
 	pub command: Command,
 }
 
-/// Benchmark a pallet or a parachain.
+/// Benchmark a pallet.
 #[derive(Subcommand)]
 pub enum Command {
 	/// Benchmark the extrinsic weight of FRAME Pallets
@@ -49,14 +49,17 @@ impl Command {
 
 		if let Some(spec) = cmd.shared_params.chain {
 			return Err(anyhow::anyhow!(format!(
-				"Chain specs are not supported. Please remove `--chain={spec}` and use \
-								`--runtime=<PATH>` instead"
+				"Chain specs are not supported. Please remove `--chain={spec}` and use `--runtime=<PATH>` instead"
 			)))?;
 		}
 		cli.warning("NOTE: this may take some time...")?;
 
-		cmd.run_with_spec::<BlakeTwo256, HostFunctions>(None)
-			.map_err(|_| anyhow::anyhow!(format!("Failed to run benchmarking for the pallet")))?;
+		cmd.run_with_spec::<BlakeTwo256, HostFunctions>(None).map_err(|e| {
+			anyhow::anyhow!(format!(
+				"Failed to run benchmarking for the pallet: {:?}",
+				e.to_string()
+			))
+		})?;
 
 		if let Some(output_path) = cmd.output {
 			cli.info(format!("Weight file is generated to {:?}", output_path.to_str()))?;
@@ -67,26 +70,38 @@ impl Command {
 
 #[cfg(test)]
 mod tests {
-	use clap::Parser;
-
 	use super::*;
+
 	use crate::cli::MockCli;
+	use clap::Parser;
+	use std::env;
 
 	#[test]
-	fn benchmark_works() -> anyhow::Result<()> {
+	fn benchmark_pallet_works() -> anyhow::Result<()> {
+		env_logger::init();
+		// Construct the path to the runtime WASM file.
+		let runtime_wasm_path =
+			env::current_dir().unwrap().join("test-resources/base_parachain.wasm");
+
 		let mut cli = MockCli::new()
 			.expect_intro("Benchmarking your pallets")
 			.expect_warning("NOTE: this may take some time...")
 			.expect_warning(
 				"NOTE: the `pop bench pallet` is not yet battle tested - double check the results.",
-			)
-			.expect_outro("Benchmark completed successfully!");
+			);
 
-		let cmd = PalletCmd::try_parse_from(&["--extrinsic=", "--pallet=", "--runtime="])?;
-		assert!(Command::bechmark_pallet(cmd, &mut cli).is_ok());
+		let cmd = PalletCmd::try_parse_from(&[
+			"",
+			"--runtime",
+			runtime_wasm_path.to_str().unwrap(),
+			"--pallet",
+			"pallet_timestamp",
+			"--extrinsic",
+			"",
+		])?;
+		Command::bechmark_pallet(cmd, &mut cli)?;
 
 		cli.verify()?;
-
 		Ok(())
 	}
 }
