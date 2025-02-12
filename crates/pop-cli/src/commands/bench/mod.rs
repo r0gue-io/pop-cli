7d3258897452c76802a0bf289f2b77453185a964
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-02-13 00:33:50 +0700

    chore: display error and refactor test file

--- crates/pop-cli/src/commands/bench/mod.rs
+++ crates/pop-cli/src/commands/bench/mod.rs
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 
-use crate::cli::{self, traits::Cli};
+use crate::{
+	cli::{self, traits::Cli},
+	common::prompt::display_message,
+};
 use clap::{Args, Subcommand};
 use frame_benchmarking_cli::PalletCmd;
-use sp_runtime::traits::BlakeTwo256;
-
-type HostFunctions = (
-	sp_statement_store::runtime_api::HostFunctions,
-	cumulus_primitives_proof_size_hostfunction::storage_proof_size::HostFunctions,
-);
+use pop_parachains::generate_benchmarks;
 
 /// Arguments for bencharmking a project.
 #[derive(Args)]
@@ -32,13 +30,13 @@ impl Command {
 	pub(crate) fn execute(args: BenchmarkArgs) -> anyhow::Result<()> {
 		let mut cli = cli::Cli;
 
-		#[cfg(feature = "parachain")]
-		match args.command {
-			Command::Pallet(cmd) => Command::bechmark_pallet(cmd, &mut cli)?,
+		let result = match args.command {
+			Command::Pallet(cmd) => Command::bechmark_pallet(cmd, &mut cli),
 		};
-
-		cli.outro("Benchmark completed successfully!")?;
-		Ok(())
+		match result {
+			Ok(()) => display_message("Benchmark completed successfully!", true, &mut cli),
+			Err(e) => display_message(&e.to_string(), false, &mut cli),
+		}
 	}
 
 	fn bechmark_pallet(cmd: PalletCmd, cli: &mut impl Cli) -> anyhow::Result<()> {
@@ -48,21 +46,27 @@ impl Command {
 		)?;
 
 		if let Some(spec) = cmd.shared_params.chain {
-			return Err(anyhow::anyhow!(format!(
-				"Chain specs are not supported. Please remove `--chain={spec}` and use `--runtime=<PATH>` instead"
-			)))?;
+			return display_message(
+				&format!(
+					"Chain specs are not supported. Please remove `--chain={spec}` \
+					       and use `--runtime=<PATH>` instead"
+				),
+				false,
+				cli,
+			);
 		}
 		cli.warning("NOTE: this may take some time...")?;
 
-		cmd.run_with_spec::<BlakeTwo256, HostFunctions>(None).map_err(|e| {
-			anyhow::anyhow!(format!(
-				"Failed to run benchmarking for the pallet: {:?}",
-				e.to_string()
-			))
-		})?;
+		if let Err(e) = generate_benchmarks(&cmd) {
+			return display_message(&e.to_string(), false, cli);
+		}
 
 		if let Some(output_path) = cmd.output {
-			cli.info(format!("Weight file is generated to {:?}", output_path.to_str()))?;
+			console::Term::stderr().clear_last_lines(1)?;
+			cli.info(format!(
+				"Weight file is generated to {}",
+				output_path.as_path().display().to_string()
+			))?;
 		}
 		Ok(())
 	}
@@ -78,30 +82,84 @@ mod tests {
 
 	#[test]
 	fn benchmark_pallet_works() -> anyhow::Result<()> {
-		env_logger::init();
-		// Construct the path to the runtime WASM file.
-		let runtime_wasm_path =
-			env::current_dir().unwrap().join("test-resources/base_parachain.wasm");
-
 		let mut cli = MockCli::new()
 			.expect_intro("Benchmarking your pallets")
-			.expect_warning("NOTE: this may take some time...")
 			.expect_warning(
 				"NOTE: the `pop bench pallet` is not yet battle tested - double check the results.",
-			);
+			)
+			.expect_warning("NOTE: this may take some time...");
 
 		let cmd = PalletCmd::try_parse_from(&[
 			"",
 			"--runtime",
-			runtime_wasm_path.to_str().unwrap(),
+			get_mock_runtime_path().to_str().unwrap(),
 			"--pallet",
 			"pallet_timestamp",
 			"--extrinsic",
 			"",
 		])?;
 		Command::bechmark_pallet(cmd, &mut cli)?;
-
 		cli.verify()?;
 		Ok(())
+	}
+
+	#[test]
+	fn benchmark_pallet_with_chainspec_fails() -> anyhow::Result<()> {
+		let spec = "path-to-chainspec";
+		let expected = format!(
+			"Chain specs are not supported. Please remove `--chain={spec}` \
+		          and use `--runtime=<PATH>` instead"
+		);
+		let mut cli = MockCli::new()
+			.expect_intro("Benchmarking your pallets")
+			.expect_warning(
+				"NOTE: the `pop bench pallet` is not yet battle tested - double check the results.",
+			)
+			.expect_outro_cancel(expected.clone());
+
+		let cmd = PalletCmd::try_parse_from(&[
+			"",
+			"--chain",
+			spec,
+			"--pallet",
+			"pallet_timestamp",
+			"--extrinsic",
+			"",
+		])?;
+
+		Command::bechmark_pallet(cmd, &mut cli)?;
+		cli.verify()?;
+		Ok(())
+	}
+
+	#[test]
+	fn benchmark_pallet_fails_with_error() -> anyhow::Result<()> {
+		let mut cli = MockCli::new()
+			.expect_intro("Benchmarking your pallets")
+			.expect_warning(
+				"NOTE: the `pop bench pallet` is not yet battle tested - double check the results.",
+			)
+			.expect_outro_cancel(format!(
+				"Failed to run benchmarking: Invalid input: No benchmarks found which match your input."
+			));
+
+		let cmd = PalletCmd::try_parse_from(&[
+			"",
+			"--runtime",
+			get_mock_runtime_path().to_str().unwrap(),
+			"--pallet",
+			"unknown-pallet-name",
+			"--extrinsic",
+			"",
+		])?;
+
+		Command::bechmark_pallet(cmd, &mut cli)?;
+		cli.verify()?;
+		Ok(())
+	}
+
+	// Construct the path to the mock runtime WASM file.
+	fn get_mock_runtime_path() -> std::path::PathBuf {
+		env::current_dir().unwrap().join("tests/files/base_parachain.wasm")
 	}
 }
