834307acd125a43007bf4825619f11692d9cadfa
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-02-17 13:15:57 +0700

    chore: clippy warning

--- crates/pop-cli/src/commands/bench/mod.rs
+++ crates/pop-cli/src/commands/bench/mod.rs
@@ -63,10 +63,7 @@ impl Command {
 
 		if let Some(output_path) = cmd.output {
 			console::Term::stderr().clear_last_lines(1)?;
-			cli.info(format!(
-				"Weight file is generated to {}",
-				output_path.as_path().display().to_string()
-			))?;
+			cli.info(format!("Weight file is generated to {}", output_path.as_path().display()))?;
 		}
 		Ok(())
 	}
