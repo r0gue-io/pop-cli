6581902551eda0a9c15d455d0e1c9c2d4b861f7f
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-02-20 10:38:05 +0700

    refactor: test files

--- crates/pop-parachains/src/bench.rs
+++ crates/pop-parachains/src/bench.rs
@@ -14,33 +14,6 @@ type HostFunctions = (
 	cumulus_primitives_proof_size_hostfunction::storage_proof_size::HostFunctions,
 );
 
-/// Run command for pallet benchmarking.
-///
-/// # Arguments
-/// * `cmd` - Command to benchmark the FRAME Pallets.
-pub fn run_pallet_benchmarking(cmd: &PalletCmd) -> Result<()> {
-	cmd.run_with_spec::<BlakeTwo256, HostFunctions>(None)
-		.map_err(|e| anyhow::anyhow!(format!("Failed to run benchmarking: {}", e.to_string())))
-}
-
-/// Parse the pallet command from string value of genesis policy builder.
-///
-/// # Arguments
-/// * `policy` - Genesis builder policy ( none | spec | runtime ).
-pub fn parse_genesis_builder_policy(policy: &str) -> anyhow::Result<PalletCmd> {
-	PalletCmd::try_parse_from([
-		"",
-		"--list",
-		"--runtime",
-		"dummy-runtime", // For parsing purpose.
-		"--genesis-builder",
-		policy,
-	])
-	.map_err(|e| {
-		anyhow::anyhow!(format!(r#"Invalid genesis builder option {policy}: {}"#, e.to_string()))
-	})
-}
-
 /// Check if a runtime has a genesis config preset.
 ///
 /// # Arguments
@@ -71,9 +44,59 @@ pub fn get_runtime_path(parent: &Path) -> anyhow::Result<PathBuf> {
 		.ok_or_else(|| anyhow::anyhow!("No runtime found."))
 }
 
+/// Parse the pallet command from string value of genesis policy builder.
+///
+/// # Arguments
+/// * `policy` - Genesis builder policy ( none | spec | runtime ).
+pub fn parse_genesis_builder_policy(policy: &str) -> anyhow::Result<PalletCmd> {
+	PalletCmd::try_parse_from([
+		"",
+		"--list",
+		"--runtime",
+		"dummy-runtime", // For parsing purpose.
+		"--genesis-builder",
+		policy,
+	])
+	.map_err(|e| {
+		anyhow::anyhow!(format!(r#"Invalid genesis builder option {policy}: {}"#, e.to_string()))
+	})
+}
+
+/// Run command for pallet benchmarking.
+///
+/// # Arguments
+/// * `cmd` - Command to benchmark the FRAME Pallets.
+pub fn run_pallet_benchmarking(cmd: &PalletCmd) -> Result<()> {
+	cmd.run_with_spec::<BlakeTwo256, HostFunctions>(None)
+		.map_err(|e| anyhow::anyhow!(format!("Failed to run benchmarking: {}", e.to_string())))
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use tempfile::tempdir;
+
+	#[test]
+	fn check_preset_works() -> anyhow::Result<()> {
+		let runtime_path = std::env::current_dir()
+			.unwrap()
+			.join("../../tests/runtimes/base_parachain_benchmark.wasm")
+			.canonicalize()?;
+		assert!(check_preset(&runtime_path, Some(&"development".to_string())).is_ok());
+		assert!(check_preset(&runtime_path, Some(&"random-preset-name".to_string())).is_err());
+		Ok(())
+	}
+
+	#[test]
+	fn get_runtime_path_works() -> anyhow::Result<()> {
+		let temp_dir = tempdir()?;
+		for name in ["runtime", "runtimes"] {
+			let path = temp_dir.path();
+			fs::create_dir(&path.join(name))?;
+			get_runtime_path(&path)?;
+		}
+		Ok(())
+	}
 
 	#[test]
 	fn parse_genesis_builder_policy_works() -> anyhow::Result<()> {
