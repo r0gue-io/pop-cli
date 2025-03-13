c8ff8a0cf3fd2994c03487c88b5b4e5b55e6e825
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-03-13 17:05:45 +0700

    chore: log command with info instead of success

--- crates/pop-cli/src/commands/bench/pallet.rs
+++ crates/pop-cli/src/commands/bench/pallet.rs
@@ -336,7 +336,7 @@ impl BenchmarkPallet {
 
 		// Display the benchmarking command.
 		cliclack::log::remark("\n")?;
-		cli.success(self.display())?;
+		cli.info(self.display())?;
 		if let Err(e) = result {
 			return display_message(&e.to_string(), false, cli);
 		}
@@ -975,9 +975,7 @@ mod tests {
 		assert!(output_path.exists());
 
 		// Verify the printed command.
-		cli = cli
-			.expect_success(cmd.display())
-			.expect_outro("Benchmark completed successfully!");
+		cli = cli.expect_info(cmd.display()).expect_outro("Benchmark completed successfully!");
 		cmd.execute(&mut cli).await?;
 		cli.verify()
 	}
