a68d5a9f84fa1807990f258ffbd7a1592c8b514e
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-03-13 15:21:41 +0700

    feat: block benchmarking

--- crates/pop-cli/src/commands/bench/mod.rs
+++ crates/pop-cli/src/commands/bench/mod.rs
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 
 use crate::cli::{self};
+use block::BenchmarkBlock;
 use clap::{Args, Subcommand};
 use pallet::BenchmarkPallet;
 use storage::BenchmarkStorage;
 
+mod block;
 mod pallet;
 mod storage;
 
@@ -19,6 +21,9 @@ pub struct BenchmarkArgs {
 /// Benchmark a pallet or a parachain.
 #[derive(Subcommand)]
 pub enum Command {
+	/// Benchmark the execution time of historic blocks.
+	#[clap(alias = "b")]
+	Block(BenchmarkBlock),
 	/// Benchmark the extrinsic weight of FRAME Pallets
 	#[clap(alias = "p")]
 	Pallet(BenchmarkPallet),
@@ -32,6 +37,7 @@ impl Command {
 	pub(crate) async fn execute(args: BenchmarkArgs) -> anyhow::Result<()> {
 		let mut cli = cli::Cli;
 		match args.command {
+			Command::Block(mut cmd) => cmd.execute(&mut cli),
 			Command::Pallet(mut cmd) => cmd.execute(&mut cli).await,
 			Command::Storage(mut cmd) => cmd.execute(&mut cli),
 		}
