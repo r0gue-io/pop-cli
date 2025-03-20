3929c8aa3da79f9bb3883cc88416ee665214788e
AlexD10S <alexfraga10@gmail.com>
2025-03-20 16:28:31 +0100

    test: include coverage for None casesin chain spec module

--- crates/pop-parachains/src/build/mod.rs
+++ crates/pop-parachains/src/build/mod.rs
@@ -714,6 +714,7 @@ mod tests {
 
 	#[test]
 	fn get_chain_name_works() -> Result<()> {
+		assert_eq!(ChainSpec(json!({})).get_name(), None);
 		let chain_spec = ChainSpec(json!({
 			"name": "test",
 		}));
@@ -732,6 +733,7 @@ mod tests {
 
 	#[test]
 	fn get_property_based_on_works() -> Result<()> {
+		assert_eq!(ChainSpec(json!({})).get_property_based_on(), None);
 		let chain_spec = ChainSpec(json!({
 			"properties": {
 				"basedOn": "test",
@@ -761,6 +763,7 @@ mod tests {
 
 	#[test]
 	fn get_sudo_key_works() -> Result<()> {
+		assert_eq!(ChainSpec(json!({})).get_sudo_key(), None);
 		let chain_spec = ChainSpec(json!({
 			"para_id": 1000,
 			"genesis": {
