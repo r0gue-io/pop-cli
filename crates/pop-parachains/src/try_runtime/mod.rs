a77aaa18578de198fb0d4e6b0a1d56a830482053
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-03-28 13:42:16 +0700

    refactor: fork State and add discriminants

--- crates/pop-parachains/src/try_runtime/mod.rs
+++ crates/pop-parachains/src/try_runtime/mod.rs
@@ -2,7 +2,7 @@ use crate::{errors::handle_command_error, Error};
 use duct::cmd;
 use frame_try_runtime::UpgradeCheckSelect;
 use std::{fmt::Display, path::PathBuf};
-use strum::Display;
+use strum::{Display, EnumDiscriminants};
 use strum_macros::{AsRefStr, EnumMessage, EnumString, VariantArray};
 
 /// Provides functionality for sourcing binaries of the `try-runtime-cli`.
@@ -23,34 +23,97 @@ impl Display for TryRuntimeCliCommand {
 	}
 }
 
-/// Subcommands for testing the runtime migrations.
-#[derive(AsRefStr, Clone, Debug, EnumString, EnumMessage, VariantArray, Eq, PartialEq, Display)]
-pub enum OnRuntimeUpgradeSubcommand {
-	/// Run the migrations of a given runtime on top of a live state.
-	#[strum(
-		serialize = "live",
-		message = "Live",
-		detailed_message = "Run the migrations of a given runtime on top of a live state."
-	)]
+/// Commands that can be executed by the `on-runtime-upgrade`.
+pub enum StateCommand {
 	Live,
-	/// Run the migrations of a given runtime on top of a chain snapshot.
-	#[strum(
-		serialize = "snapshot",
-		message = "Snapshot",
-		detailed_message = "Run the migrations of a given runtime on top of a chain snapshot."
-	)]
 	Snapshot,
 }
 
-impl OnRuntimeUpgradeSubcommand {
+impl Display for StateCommand {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		let s = match self {
+			StateCommand::Live => "live",
+			StateCommand::Snapshot => "snap",
+		};
+		write!(f, "{}", s)
+	}
+}
+
+/// The source of runtime *state* to use.
+#[derive(Clone, Debug, Display, clap::Subcommand, EnumDiscriminants)]
+#[strum_discriminants(derive(AsRefStr, EnumString, EnumMessage, VariantArray))]
+pub enum State {
+	/// Use a state snapshot as the source of runtime state.
+	#[strum_discriminants(strum(
+		serialize = "snapshot",
+		message = "Snapshot",
+		detailed_message = "Run the migrations of a given runtime on top of a chain snapshot."
+	))]
+	Snap {
+		#[clap(short = 'p', long = "path", alias = "snapshot-path")]
+		path: Option<PathBuf>,
+	},
+
+	/// Use a live chain as the source of runtime state.
+	#[strum_discriminants(strum(
+		serialize = "live",
+		message = "Live",
+		detailed_message = "Run the migrations of a given runtime on top of a live state."
+	))]
+	Live(LiveState),
+}
+
+impl State {
 	/// Get the command string for the `on-runtime-upgrade` subcommand.
 	pub fn command(&self) -> String {
 		match self {
-			OnRuntimeUpgradeSubcommand::Live => "live",
-			OnRuntimeUpgradeSubcommand::Snapshot => "snap",
+			State::Live(..) => StateCommand::Live,
+			State::Snap { .. } => StateCommand::Snapshot,
 		}
 		.to_string()
 	}
+}
+
+/// A `Live` variant for [`State`]
+#[derive(Debug, Clone, clap::Args)]
+pub struct LiveState {
+	/// The url to connect to.
+	#[arg(
+		short,
+		long,
+		value_parser = check_url,
+	)]
+	pub uri: Option<String>,
+
+	/// The block hash at which to fetch the state.
+	///
+	/// If non provided, then the latest finalized head is used.
+	#[arg(
+		short,
+		long,
+		value_parser = check_block_hash,
+	)]
+	pub at: Option<String>,
+
+	/// A pallet to scrape. Can be provided multiple times. If empty, entire chain state will
+	/// be scraped.
+	///
+	/// This is equivalent to passing `xx_hash_64(pallet)` to `--hashed_prefixes`.
+	#[arg(short, long, num_args = 1..)]
+	pub pallet: Vec<String>,
+
+	/// Storage entry key prefixes to scrape and inject into the test externalities. Pass as 0x
+	/// prefixed hex strings. By default, all keys are scraped and included.
+	#[arg(long = "prefix", value_parser = check_block_hash, num_args = 1..)]
+	pub hashed_prefixes: Vec<String>,
+
+	/// Fetch the child-keys as well.
+	///
+	/// Default is `false`, if specific `--pallets` are specified, `true` otherwise. In other
+	/// words, if you scrape the whole state the child tree data is included out of the box.
+	/// Otherwise, it must be enabled explicitly using this flag.
+	#[arg(long)]
+	pub child_tree: bool,
 }
 
 /// Get the details of upgrade checks options for testing the runtime migrations.
@@ -109,4 +172,53 @@ pub fn generate_try_runtime(
 		println!("{}", String::from_utf8_lossy(&output.stderr));
 	}
 	Ok(())
+}
+
+/// Checks if the given string is a valid URL.
+///
+/// # Arguments
+///
+/// * `s` - The string to check.
+pub fn check_url(s: &str) -> Result<String, &'static str> {
+	if s.starts_with("ws://") || s.starts_with("wss://") {
+		// could use Url crate as well, but lets keep it simple for now.
+		Ok(s.to_string())
+	} else {
+		Err("not a valid WS(S) url: must start with 'ws://' or 'wss://'")
+	}
+}
+
+/// Checks if the given string is a valid block hash.
+///
+/// # Arguments
+///
+/// * `block_hash` - The string to check.
+pub fn check_block_hash(block_hash: &str) -> anyhow::Result<String> {
+	let (block_hash, offset) = if let Some(block_hash) = block_hash.strip_prefix("0x") {
+		(block_hash, 2)
+	} else {
+		(block_hash, 0)
+	};
+
+	if let Some(pos) = block_hash.chars().position(|c| !c.is_ascii_hexdigit()) {
+		Err(anyhow::anyhow!(
+			"Expected block hash, found illegal hex character at position: {}",
+			offset + pos,
+		))
+	} else {
+		Ok(block_hash.into())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn check_block_hash_works() {
+		assert!(check_block_hash("0x1234567890abcdef").is_ok());
+		assert!(check_block_hash("1234567890abcdef").is_ok());
+		assert!(check_block_hash("0x1234567890abcdefg").is_err());
+		assert!(check_block_hash("1234567890abcdefg").is_err());
+	}
 }
