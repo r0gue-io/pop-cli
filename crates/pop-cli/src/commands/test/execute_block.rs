39de39c5311374f04d8d5d65888ee8627021e24f
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-04-01 00:15:59 +0700

    chore: fix test

--- crates/pop-cli/src/commands/test/execute_block.rs
+++ crates/pop-cli/src/commands/test/execute_block.rs
@@ -53,10 +53,8 @@ impl TestExecuteBlockCommand {
 		let result = self.run(cli).await;
 
 		// Display the `execute-block` command.
-		println!("{}", self.display()?);
 		cli.info(self.display()?)?;
 		if let Err(e) = result {
-			println!("e: {:?}", e);
 			return display_message(&e.to_string(), false, cli);
 		}
 		display_message("Tested block execution successfully!", true, cli)
@@ -151,8 +149,13 @@ mod tests {
 				"pop test execute-block --try-state={} --uri={}",
 				parse_try_state_string(TryStateSelect::None)?,
 				DEFAULT_LIVE_NODE_URL,
-			));
-		TestExecuteBlockCommand::default().execute(&mut cli).await?;
+			))
+			.expect_outr_cancel("thread 'main' panicked at cli/main.rs:326:10:\n\
+			called `Result::unwrap()` on an `Err` value: Input(\"Given runtime is not compiled with the try-runtime feature.\")\n\
+			note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace");
+		let error = TestExecuteBlockCommand::default().execute(&mut cli).await.unwrap_err();
+		// The error is expected because `pop-node` production runtime on Paseo is not compiled with the `try-runtime` feature.
+		assert!(error.to_string().contains("Given runtime is not compiled with the try-runtime feature."));
 		cli.verify()
 	}
 
