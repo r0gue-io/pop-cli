99caaef571dfb283b9d140d5334f0da342b31077
Frank Bell <frank@r0gue.io>
2025-04-11 12:51:47 +0100

    test(parachains): update integration tests after parameter type change

--- crates/pop-parachains/tests/parachain.rs
+++ crates/pop-parachains/tests/parachain.rs
@@ -2,6 +2,7 @@
 
 use anyhow::Result;
 use pop_parachains::Zombienet;
+use std::path::Path;
 
 const BINARY_VERSION: &str = "v1.13.0";
 
@@ -12,7 +13,7 @@ async fn launch_kusama() -> Result<()> {
 
 	let mut zombienet = Zombienet::new(
 		&cache,
-		"../../tests/networks/kusama.toml",
+		Path::new("../../tests/networks/kusama.toml"),
 		Some(BINARY_VERSION),
 		Some("v1.2.7"),
 		None,
@@ -36,7 +37,7 @@ async fn launch_paseo() -> Result<()> {
 
 	let mut zombienet = Zombienet::new(
 		&cache,
-		"../../tests/networks/paseo.toml",
+		Path::new("../../tests/networks/paseo.toml"),
 		Some(BINARY_VERSION),
 		Some("v1.2.4"),
 		None,
@@ -60,7 +61,7 @@ async fn launch_polkadot() -> Result<()> {
 
 	let mut zombienet = Zombienet::new(
 		&cache,
-		"../../tests/networks/polkadot.toml",
+		Path::new("../../tests/networks/polkadot.toml"),
 		Some(BINARY_VERSION),
 		Some("v1.2.7"),
 		None,
@@ -84,7 +85,7 @@ async fn launch_polkadot_and_system_parachain() -> Result<()> {
 
 	let mut zombienet = Zombienet::new(
 		&cache,
-		"../../tests/networks/polkadot+collectives.toml",
+		Path::new("../../tests/networks/polkadot+collectives.toml"),
 		Some(BINARY_VERSION),
 		Some("v1.2.7"),
 		Some(BINARY_VERSION),
@@ -108,7 +109,7 @@ async fn launch_paseo_and_system_parachain() -> Result<()> {
 
 	let mut zombienet = Zombienet::new(
 		&cache,
-		"../../tests/networks/paseo+coretime.toml",
+		Path::new("../../tests/networks/paseo+coretime.toml"),
 		Some(BINARY_VERSION),
 		None,
 		Some(BINARY_VERSION),
@@ -132,7 +133,7 @@ async fn launch_paseo_and_two_parachains() -> Result<()> {
 
 	let mut zombienet = Zombienet::new(
 		&cache,
-		"../../tests/networks/pop.toml",
+		Path::new("../../tests/networks/pop.toml"),
 		Some(BINARY_VERSION),
 		None,
 		Some(BINARY_VERSION),
