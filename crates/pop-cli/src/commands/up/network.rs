69a7007e4d9dbcfc85214f8d3b2bb73f881311c4
José Molina <jose@blockdeep.io>
2025-08-15 10:38:37 +0200

    Improve misleading reference to cache

--- crates/pop-cli/src/commands/up/network.rs
+++ crates/pop-cli/src/commands/up/network.rs
@@ -444,9 +444,9 @@ pub(crate) async fn spawn(
 			tokio::signal::ctrl_c().await?;
 
 			if auto_remove {
-				// Remove cache directory after network is terminated
+				// Remove zombienet directory after network is terminated
 				if let Err(e) = std::fs::remove_dir_all(&base_dir) {
-					cli.warning(format!("🚫 Failed to remove cache directory: {e}"))?;
+					cli.warning(format!("🚫 Failed to remove zombienet directory: {e}"))?;
 				}
 			}
 
