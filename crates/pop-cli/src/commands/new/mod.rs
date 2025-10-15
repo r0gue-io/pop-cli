0b778c8c41cbbe94a6144fc44f87933d44bfdd3d
AlexD10S <alexfraga10@gmail.com>
2025-10-15 16:47:58 +0200

    feat: prompt the user in the interactive guide

--- crates/pop-cli/src/commands/new/mod.rs
+++ crates/pop-cli/src/commands/new/mod.rs
@@ -110,7 +110,6 @@ pub fn guide_user_to_select_command(cli: &mut impl cli::traits::Cli) -> AnyhowRe
 			decimals: None,
 			initial_endowment: None,
 			verify: false,
-			// TODO
 			with_frontend: false,
 		})),
 		#[cfg(feature = "chain")]
@@ -125,7 +124,6 @@ pub fn guide_user_to_select_command(cli: &mut impl cli::traits::Cli) -> AnyhowRe
 			name: None,
 			contract_type: None,
 			template: None,
-			// TODO
 			with_frontend: false,
 		})),
 		_ => Err(anyhow::anyhow!("Invalid selection")),
